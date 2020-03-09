2a75b55fa157c3c2f1e17d6a6057c5e0f9390a4b
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-03-09 18:22:33 +0200

    Added Categories ViewModel and a Categories View with name SortByName.cshtml. Also added a Categories Service to Services.Data.

---
+++ ASP.NET Core/Web/MyForumApp.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MyForumApp.Web.ViewModels.Categories
+{
+    using System;
+
+    using MyForumApp.Data.Models;
+    using MyForumApp.Services.Mapping;
+
+    public class PostInCategoryViewModel : IMapFrom<Post>
+    {
+        public DateTime CreatedOn { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortContent =>
+            this.Content?.Length > 100
+                ? this.Content?.Substring(0, 100) + "..."
+                : this.Content;
+
+        public string UserUserName { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
