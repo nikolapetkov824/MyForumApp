7e2a2bfbb4724745ebb853c5d28cb0eac47365ff
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-03-09 21:26:45 +0200

    Added Category View Model to View Models.

---
+++ ASP.NET Core/Web/MyForumApp.Web.ViewModels/Home/IndexCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MyForumApp.Web.ViewModels.Home
+{
+    using MyForumApp.Data.Models;
+    using MyForumApp.Services.Mapping;
+
+    public class IndexCategoryViewModel : IMapFrom<Category>
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int PostsCount { get; set; }
+
+        public string Url => $"/f/{this.Name.Replace(' ', '-')}";
+    }
+}
