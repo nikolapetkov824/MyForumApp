99aec8b8bbab15316f94a323d414828f4cee21f9
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-03-19 21:01:19 +0200

    Added IVotesService.cs

---
+++ ASP.NET Core/Services/MyForumApp.Services.Data/IVotesService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyForumApp.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IVotesService
+    {
+        Task VoteAsync(int postId, string userId, bool isUpVote);
+    }
+}
