783199b73168698101f4c58de7ee207aae3f6185
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-03-26 17:05:57 +0200

    Added migration AddReplies

---
+++ ASP.NET Core/Data/MyForumApp.Data/Migrations/20200326150456_AddReplies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyForumApp.Data.Migrations
+{
+    public partial class AddReplies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
