7914e86ad89ab9c6ac0efdc178bb7fb42e1fb4fa
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-04-06 14:41:38 +0300

    Added DefaultImageUrl for users

--- ASP.NET Core/Web/MyForumApp.Web.ViewModels/Users/UserRegisterViewModel.cs
+++ ASP.NET Core/Web/MyForumApp.Web.ViewModels/Users/UserRegisterViewModel.cs
@@ -14,5 +14,7 @@
         public string ConfirmPassword { get; set; }
 
         public string ImageUrl { get; set; }
+
+        public string DefaultImageUrl => "https://www.4dface.io/wp-content/uploads/2018/10/4DFM_sample2.jpg";
     }
 }
