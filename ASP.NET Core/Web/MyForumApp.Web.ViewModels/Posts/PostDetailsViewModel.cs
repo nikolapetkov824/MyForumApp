b98eff79efaa85931bdc7e741b6faca40d8de64e
nikolapetkov824 <43876450+nikolapetkov824@users.noreply.github.com>
2020-04-07 16:53:18 +0300

    New PostDetailsController.cs. Added Details button to every Post

---
+++ ASP.NET Core/Web/MyForumApp.Web.ViewModels/Posts/PostDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Ganss.XSS;
+using MyForumApp.Data.Models;
+using MyForumApp.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyForumApp.Web.ViewModels.Posts
+{
+    public class PostDetailsViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string SanitizedContent => new HtmlSanitizer().Sanitize(this.Description);
+
+        public string UserUserName { get; set; }
+
+        public int VotesCount { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
---
+++ ASP.NET Core/Web/MyForumApp.Web/Controllers/PostDetailsController.cs
@@ -0,0 +1,50 @@
+﻿namespace MyForumApp.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using MyForumApp.Data;
+    using MyForumApp.Data.Models;
+    using MyForumApp.Services.Data;
+    using MyForumApp.Web.ViewModels.Posts;
+    using MyForumApp.Web.ViewModels.Replies;
+
+    public class PostDetailsController : Controller
+    {
+        private readonly IPostsService postsService;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ApplicationDbContext dbContext;
+
+        public PostDetailsController(
+            IPostsService postsService,
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext dbContext)
+        {
+            this.postsService = postsService;
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult GetById(int postId)
+        {
+            var post = this.postsService.GetPostDetails<PostDetailsViewModel>(postId);
+
+            //var viewModel = new PostDetailsViewModel
+            //{
+            //    CategoryName = post.CategoryName,
+            //    CreatedOn = post.CreatedOn,
+            //    Title = post.Title,
+            //    Description = post.Description,
+            //    UserUserName = post.UserUserName,
+            //    VotesCount = post.VotesCount,
+            //    CommentsCount = post.CommentsCount,
+            //};
+
+            return this.View(post);
+        }
+    }
+}
